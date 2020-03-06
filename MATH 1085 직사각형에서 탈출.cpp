ce3016cfe4ab115113eb680c5e2c6f3f9701dab4
minjeong <mathmjseo@naver.com>
2020-03-06 22:26:16 +0900

    revise code 20/03/06
    revise code

--- MATH 1085 직사각형에서 탈출.cpp
+++ MATH 1085 직사각형에서 탈출.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 int main(void){
     int x, y, w, h;
     cin >> x >> y >> w >> h;
-    if(w-x > h-y) cout << h-y << "\n";
-    else cout << w-x << "\n";
+
+    int step = min(w-x, x);
+    step = min(step,min(h-y,y));
+    cout << step << "\n";
+
     return 0;
 }
--- MATH 1912 연속합.cpp
+++ MATH 1912 연속합.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
+
 int main(void){
-    int n, tmp;
+    int n;
     cin >> n;
-    int sum = 0, max = 0;
+    int dp[n], se[n];
     for(int i = 0; i < n; i++){
-        cin >> tmp;
-        if(tmp > 0) sum += tmp;
-        if(max < sum) max = sum;
-        if(tmp < 0 && i != 0) sum = 0;
+        cin >> dp[i];
+        se[i] = dp[i];
     }
-    cout << max << "\n";
+
+    int sum = 0, maxSum = dp[0];
+    for(int i = 1; i < n; i++){
+        dp[i] = max(dp[i],dp[i-1]+se[i]);
+        if(dp[i] > maxSum) maxSum = dp[i];
+    }
+
+    cout << maxSum << "\n";
     return 0;
 }
