f2441a66cfa07ae765cd3bdc09e31d7114ebd360
seovalue <seominjeong.dev@gmail.com>
2021-06-27 00:31:21 +0900

    solve: 124 나라의 숫자

---
+++ before/java-code/prg_124나라의숫자.java
@@ -0,0 +1,40 @@
+// 내 풀이
+import java.util.*;
+
+class Solution {
+    private static final List<String> BASE_NUMBER = Arrays.asList("4", "1", "2");
+    public String solution(int n) {
+        List<String> answers = new ArrayList<>();
+        while (n != 0) {
+            answers.add(0, divide(n));
+            n = (n-1) / 3;
+        }
+        return String.join("",answers);
+    }
+    
+    private String divide(int n) {
+        return BASE_NUMBER.get(n%3);
+    }
+}
+
+
+// 다른 사람의 풀이
+class Solution {
+  public String solution(int n) {
+      String[] numbers = {"4", "1", "2"};
+      String answer = "";
+      
+      int num = n;
+      
+      while(num > 0){
+          int remainder = num % 3;
+          num /= 3;
+          
+          if(remainder == 0) num--;
+          
+          answer = numbers[remainder] + answer;
+      }
+      
+      return answer;
+  }
+}
