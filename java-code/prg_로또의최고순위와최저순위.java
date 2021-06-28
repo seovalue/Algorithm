21bd8af87b70c3cc4158e7dd32ae01303ef6d82e
seovalue <seominjeong.dev@gmail.com>
2021-06-28 23:38:33 +0900

    solve: 로또의 최고 순위와 최저 순위

---
+++ java-code/prg_로또의최고순위와최저순위.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = new int[]{1, 6}; // 모두 0인 경우를 default로 둔다.
+        List<Integer> lottoList = new ArrayList<>();
+        for (int lotto : lottos) {
+            lottoList.add(lotto);
+        }
+        final int size = lottoList.size();
+        final int zeros = Collections.frequency(lottoList, 0);
+        
+        if (zeros == size) {
+            return answer;
+        }
+        
+        int lowestRank = 0;
+        for (int win_num : win_nums) {
+            if (lottoList.contains(win_num)) { // 우승 번호를 갖고 있다면
+                lowestRank += 1;
+            }
+        }
+        final int highestRank = zeros + lowestRank;
+        answer[0] = convertToRank(highestRank);
+        answer[1] = convertToRank(lowestRank);
+        return answer;
+    }
+    
+    private int convertToRank(final int counts) {
+        if (counts == 1 || counts == 0) {
+            return 6;
+        }
+        return 7 - counts;
+    }
+}
