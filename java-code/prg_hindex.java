8f0be40add26cb12bf7a5d0c12f31b7fc725afa3
seovalue <seominjeong.dev@gmail.com>
2021-07-05 23:55:19 +0900

    solve: h-index

---
+++ java-code/prg_hindex.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] citations) {
+        Arrays.sort(citations); // 오름차순으로 정렬
+        int size = citations.length; 
+        int start = 0, end = size - 1; // start와 end는 인덱스로 가져간다.
+        while (start <= end) { 
+            int mid = (start + end) / 2;
+            /**
+            h번 이상 인용된 논문이 h편 이상인 h의 최댓값
+            */
+            int h = size - mid;
+            if (citations[mid] < h) start = mid + 1;
+            else {
+                if (mid > 0 && citations[mid] <= h){
+                    return h;
+                }
+                end = mid - 1;
+            }
+        }
+        if (start >= size) {
+            return 0;
+        }
+        if (citations[start] >= size - start) return size - start;
+        if (citations[end] >= size - end) return size - end;
+        return 0;
+    }
+}
