6ba954a691e576e7f69ea419bcb81af39b4d0569
서민정 <seominjeong.dev@gmail.com>
2021-10-02 01:02:26 +0900

    solve: 완주하지 못한 선수

---
+++ java-code/prg_완주하지못한선수2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        // players 맵 초기화
+        Map<String, Integer> player = new HashMap<>();
+        for (String p: participant) {
+            if (player.containsKey(p)) {
+                player.put(p, player.get(p)+1);
+            } else {
+                player.put(p, 1);
+            }
+        }
+        
+        for (String c: completion) {
+            player.put(c, player.get(c)-1);
+        }
+        
+        for (Map.Entry<String, Integer> entry: player.entrySet()) {
+            if (entry.getValue() > 0) {
+                answer = entry.getKey();
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
