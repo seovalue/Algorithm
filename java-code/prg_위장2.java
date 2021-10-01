02b149b713e3b5c70dd0232c9869a0cffe1233f7
서민정 <seominjeong.dev@gmail.com>
2021-10-02 01:23:15 +0900

    solve: 위장

---
+++ java-code/prg_위장2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String[] c: clothes) {
+            String key = c[1];
+            String value = c[0];
+            
+            if (map.containsKey(key)) {
+                map.put(key, map.get(key) + 1);
+            } else {
+                map.put(key, 1);
+            }
+        }
+        
+        int answer = 1;
+        for (Integer value: map.values()) {
+            answer *= (value + 1);
+        }
+        return answer - 1;
+    }
+}
